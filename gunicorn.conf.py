648fb495d4e9d778118f1498c3b1f3b3d4c9b6f8
Andrew McPhail <andrew@cgwm.co.za>
2021-04-26 10:16:58 +0000

    server change

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 command = '/apps/wbdesigner/.venv/bin/gunicorn'
-pythonpath = '/apps/wbdesigner/pdm_project_one', '/apps/wbdesigner/.venv/bin/'
+pythonpath = '/apps/wbdesigner/pdm_project_one,/apps/wbdesigner/.venv/bin/'
 bind = '127.0.0.1:8000'
 workers = 4
 worker_class = 'uvicorn.workers.UvicornWorker'
--- pdm_project_one/api/viewmodels.py
+++ pdm_project_one/api/viewmodels.py
@@ -17,15 +17,14 @@ class DesignViewModel(ViewModelBase):
 
     def __init__(self, request: Request):
         super().__init__(request)
-
         self.email = ''
         self.final_text = ''
         self.final_image = None
 
-        def load():
-            form = self.request.form()
-            self.email = form.get('email').lower().strip()
-            self.final_text = form.get('wbtext').upper().strip()
-            self.final_image = form.get('upload-file')
+    def load(self):
+        form = self.request.form()
+        self.email = form.get('email').lower().strip()
+        self.final_text = form.get('wbtext').upper().strip()
+        self.final_image = form.get('upload-file')
 
 
