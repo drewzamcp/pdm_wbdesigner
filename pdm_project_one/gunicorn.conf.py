ef1d0573601017de34b03d3c985426d4cfbe3f6f
Andrew McPhail <azwerd@Andrews-MacBook-Pro.local>
2021-05-18 14:30:39 +0200

    config changes to nginx and gunicorn

--- pdm_project_one/gunicorn.conf.py
+++ pdm_project_one/gunicorn.conf.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-command = '/apps/wbdesigner/.venv/bin/gunicorn'
+command = '/home/azwerd/webapp/.venv/bin/gunicorn'
 bind = '127.0.0.1:8000'
 workers = 4
 worker_class = 'uvicorn.workers.UvicornWorker'
-user = 'wbduser'
-chdir = '/apps/wbdesigner/pdm_project_one'
+user = 'azwerd'
+chdir = '/home/azwerd/webapp/pdm_project_one'
